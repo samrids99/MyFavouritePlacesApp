a35c78240f80a143e2c1bdcf56310582fcf80402
Sam Ridsdale <samridsdale108@gmail.com>
2026-09-22 20:25:39 +0100

    adding an add place screen + navigation

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AllPlaces from "./screens/AllPlaces";
+import AddPlace from "./screens/AddPlace";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="AllPlaces" component={AllPlaces} />
+          <Stack.Screen name="AddPlace" component={AddPlace} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
