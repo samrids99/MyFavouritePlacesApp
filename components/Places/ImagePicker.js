2966bfe1bbbe21971ec7034524738a7e0439d710
Sam Ridsdale <samridsdale108@gmail.com>
2026-09-23 17:17:04 +0100

    showing an image preview

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -43,8 +43,7 @@ function ImagePicker() {
       aspect: [16, 9],
       quality: 0.5,
     });
-
-    setPickedImage(image.uri);
+    setPickedImage(image.assets[0].uri);
   }
 
   let imagePreview = <Text>No Image Taken...</Text>;
